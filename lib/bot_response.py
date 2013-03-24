edb068dbf03a0704c83b00f459dbeb3e11b866df
rhea <rhea@rhea.(none)>
2013-03-23 22:49:19 -0400

    edit

--- lib/bot_response.py
+++ lib/bot_response.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from string import punctuation
 import random
 import os
 
@@ -7,7 +8,7 @@ import os
 def response():
     with open(os.path.abspath('') + '/lib/steele1983.txt') as f:
         text = f.read()
-    text = [i for i in text.lower().split() if i.isalpha()]
+    text = [i.strip(punctuation) for i in text.lower().split() if i.isalpha()]
     markov = {}
     sentence_length = random.randint(15, 20)
     for (w1, w2) in zip(text[:-1], text[1:]):
