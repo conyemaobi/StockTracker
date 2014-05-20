65d1558e3b761ace3efe4b8080061b7e75f20c52
Joe Cabrera <calcmaster16@gmail.com>
2014-05-20 01:46:04 +0000

    stock checking

--- lib/bot.py
+++ lib/bot.py
@@ -6,10 +6,27 @@ from twisted.words.protocols import irc
 #from bot_response import response
 import re
 import psycopg2
+import urllib2
+import json
+
+exchanges = ["NASDAQ", "NYSE"]
 
 # our connection
 cur = conn.cursor()
 print "Opened database successfully"
+
+def is_valid_stock(ticker):
+	url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query="+str(ticker)+"&callback=YAHOO.Finance.SymbolSuggest.ssCallback"
+	response = urllib2.urlopen(url)
+	html = response.read().lstrip("YAHOO.Finance.SymbolSuggest.ssCallback(").rstrip(")")
+	data = json.loads(html)
+	try:
+		if data["ResultSet"]["Result"][0]["typeDisp"] == "Equity" and data["ResultSet"]["Result"][0]["exchDisp"] in exchanges:
+			return True
+		else:
+			return False
+	except IndexError:
+		pass
 
 class Bot(irc.IRCClient):
     nickname = raw_input('Name your bot: ')
@@ -49,21 +66,23 @@ class Bot(irc.IRCClient):
         user = user.split('!')[0]
         print '%s | %s' % (user, msg)
 	regex = re.compile("[A-Z]+\s\(")
+	#regex = re.compile("[A-Z]+\s")
 	tickers = regex.findall(msg)
 	for t in tickers:
 		t = t.strip('(')
-		cur.execute("SELECT count(*) from STOCKS where stock=\'"+str(t)+"\'")
-		rows = cur.fetchall()
-		for row in rows:
-			if int(row[0]) > 0:
-				cur.execute("UPDATE STOCKS SET count = count + 1 WHERE stock =\'"+str(t)+"\'")
-				conn.commit()
-				cur.execute("UPDATE STOCKS SET last_modified = current_timestamp WHERE stock =\'"+str(t)+"\'")
-				conn.commit()
-			else:
-				cur.execute("INSERT INTO STOCKS (STOCK,COUNT,LAST_MODIFIED) VALUES (\'"+str(t)+"\',1,current_timestamp)");
-				conn.commit()
-			print "Records created successfully";
+		if is_valid_stock(t.replace(" ", "")):
+			cur.execute("SELECT count(*) from STOCKS where stock=\'"+str(t)+"\'")
+			rows = cur.fetchall()
+			for row in rows:
+				if int(row[0]) > 0:
+					cur.execute("UPDATE STOCKS SET count = count + 1 WHERE stock =\'"+str(t)+"\'")
+					conn.commit()
+					cur.execute("UPDATE STOCKS SET last_modified = current_timestamp WHERE stock =\'"+str(t)+"\'")
+					conn.commit()
+				else:
+					cur.execute("INSERT INTO STOCKS (STOCK,COUNT,LAST_MODIFIED) VALUES (\'"+str(t)+"\',1,current_timestamp)");
+					conn.commit()
+				print "Records created successfully";
 	#conn.close()
         #answer = response()
 	#answer = t.strip('(')
